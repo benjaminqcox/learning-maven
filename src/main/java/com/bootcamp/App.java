3fbb502d50317ecd68cef429c53be0e5ba08ce56
benjaminqcox <benjaminqcox@gmail.com>
2023-07-26 11:27:10 +0100

    Added local maven package (nothelloworld as seen in pom.xml)

--- src/main/java/com/bootcamp/App.java
+++ src/main/java/com/bootcamp/App.java
@@ -1,4 +1,5 @@
 package com.bootcamp;
+import com.skybootcamp.working;
 
 /**
  * Hello world!
@@ -9,5 +10,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(working.testing());
     }
 }
